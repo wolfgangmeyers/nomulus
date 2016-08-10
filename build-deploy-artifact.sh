fd14d5f193951181c13b9337fc4ff7d58731c909
Hans Ridder <hans@donuts.co>
2016-08-10 10:58:06 -0700

    Change build script to new target and .ear file

--- build-deploy-artifact.sh
+++ build-deploy-artifact.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-bazel build --javacopt "-source 1.7" --javacopt "-target 1.7" //java/google/registry/env:production_ear
-OK=$?
-if [ $OK -eq 0 ]
-then
- echo "Extracting ear file"
-else
- echo "Build failed"
- exit 1
+
+if [ -z "$1" ]; then
+  echo ".ear directory required"
+  exit 1
+fi
+EAR_DIR=$1
+
+if ! bazel build --javacopt "-source 1.7" --javacopt "-target 1.7" //java/google/registry:registry.ear; then
+  echo "Build failed"
+  exit 1
 fi
 
-rm -rf mercury-donuts
-mkdir mercury-donuts
-cd mercury-donuts
-jar -xf ../bazel-genfiles/java/google/registry/env/production_ear.ear
-cd ../
+echo "Extracting .ear file to" $PWD/$EAR_DIR
+rm -rf $EAR_DIR
+mkdir $EAR_DIR
+cd $EAR_DIR
+jar -xf ../bazel-genfiles/java/google/registry/registry.ear
+cd $OLDPWD
