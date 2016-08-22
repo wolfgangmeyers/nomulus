166871fce7edb8d04edcd807d56dfe2d5e2446c3
David Drake <yurodivuie@gmail.com>
2016-08-22 14:57:14 -0700

    Updated the build-deploy-artifact.sh to allow for a second environmen… (#31)
    * Updated the build-deploy-artifact.sh to allow for a second environments, supplying an environment name.
    Updated the crash config to match production more closely.
    
    * Added better handling for production environment

--- build-deploy-artifact.sh
+++ build-deploy-artifact.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo ".ear directory required"
+  echo "usage: build-deploy-artifact.sh ear_dir [environment]"
+  echo "    ear_dir: Directory where the .ear file will be expanded.  Required."
+  echo "    environment: The environment the build should target (\"crash\", e.g.).  Defaults to production environment."
   exit 1
 fi
 EAR_DIR=$1
 
-if ! bazel build --javacopt "-source 1.7" --javacopt "-target 1.7" //java/google/registry:registry.ear; then
+ENV_SUFFIX=""
+if [ -n "$2" ]; then
+  echo "building for $2 environment"
+  if [ "$2" != "production" ]; then
+    ENV_SUFFIX="_$2"
+  fi
+fi
+
+if ! bazel build --javacopt "-source 1.7" --javacopt "-target 1.7" //java/google/registry:registry${ENV_SUFFIX}.ear; then
   echo "Build failed"
   exit 1
 fi
@@ -15,5 +25,5 @@ echo "Extracting .ear file to" $PWD/$EAR_DIR
 rm -rf $EAR_DIR
 mkdir $EAR_DIR
 cd $EAR_DIR
-jar -xf ../bazel-genfiles/java/google/registry/registry.ear
+jar -xf ../bazel-genfiles/java/google/registry/registry${ENV_SUFFIX}.ear
 cd $OLDPWD
